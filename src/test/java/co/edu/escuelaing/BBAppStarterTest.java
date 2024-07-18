2ecf8297497389026b2c588567dc34a4dc335e5a
unknown <hann.jang@labinfo.is.escuelaing.edu.co>
2024-07-18 12:31:44 -0500

    Avance

---
+++ src/test/java/co/edu/escuelaing/BBAppStarterTest.java
@@ -0,0 +1,36 @@
+package co.edu.escuelaing;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BBAppStarterTest extends TestCase{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BBAppStarterTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BBAppStarterTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
